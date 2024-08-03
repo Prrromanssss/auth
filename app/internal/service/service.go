d4817daad5dc3957f9e98e064e9f383367cbd259
sourcream <b.roman.a@crtex.io>
2024-08-03 09:42:46 +0300

    make service layer

--- app/internal/service/service.go
+++ app/internal/service/service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"context"
+
+	"github.com/Prrromanssss/auth/internal/model"
+)
+
+// UserRepository defines methods for user data operations.
+type UserService interface {
+	// CreateUser adds a new user and returns the user ID and any error.
+	CreateUser(ctx context.Context, params *model.CreateUserParams) (resp *model.CreateUserResponse, err error)
+
+	// GetUser retrieves a user by ID and returns user details and any error.
+	GetUser(ctx context.Context, params *model.GetUserParams) (resp *model.GetUserResponse, err error)
+
+	// UpdateUser updates user details by ID and returns any error.
+	UpdateUser(ctx context.Context, params *model.UpdateUserParams) (err error)
+
+	// DeleteUser removes a user by ID and returns any error.
+	DeleteUser(ctx context.Context, params *model.DeleteUserParams) (err error)
+}
--- app/internal/service/user/service.go
+++ app/internal/service/user/service.go
@@ -1,1 +1,69 @@
 package user
+
+import (
+	"context"
+
+	"github.com/Prrromanssss/auth/internal/model"
+	"github.com/Prrromanssss/auth/internal/repository"
+	"github.com/Prrromanssss/auth/internal/service"
+)
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+func NewService(
+	userRepository repository.UserRepository,
+) service.UserService {
+	return &userService{
+		userRepository: userRepository,
+	}
+}
+
+func (s *userService) CreateUser(
+	ctx context.Context,
+	params *model.CreateUserParams,
+) (resp *model.CreateUserResponse, err error) {
+	resp, err = s.userRepository.CreateUser(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (s *userService) GetUser(
+	ctx context.Context,
+	params *model.GetUserParams,
+) (resp *model.GetUserResponse, err error) {
+	resp, err = s.userRepository.GetUser(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (s *userService) UpdateUser(
+	ctx context.Context,
+	params *model.UpdateUserParams,
+) (err error) {
+	err = s.userRepository.UpdateUser(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *userService) DeleteUser(
+	ctx context.Context,
+	params *model.DeleteUserParams,
+) (err error) {
+	err = s.userRepository.DeleteUser(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
